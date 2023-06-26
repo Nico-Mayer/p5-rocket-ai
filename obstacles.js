710dfd16ef16e5620517a53c69c13dffeab10873
Nico-Mayer <nico.m94@icloud.com>
2023-06-26 08:29:00 +0200

    small changes

--- obstacles.js
+++ obstacles.js
@@ -1,28 +1,13 @@
 function createObstacles(w, h) {
-  obstacles[0] = new Obstacle(
-    "RECT",
-    w / 2 - (w * 0.45) / 2,
-    h * 0.35,
-    w * 0.45,
-    35
-  );
-  obstacles[1] = new Obstacle("RECT", w - 400, h * 0.55, 190, 35);
-  obstacles[2] = new Obstacle("RECT", 200, h * 0.25, 270, 35);
-  obstacles[3] = new Obstacle("RECT", 900, 50, 30, 190);
-  obstacles[4] = new Obstacle("ELLIPSE", w / 2, h * 0.75, 70, 70);
-
-  /* obstacles[0] = new Obstacle(
-      "RECT",
-      0,
-      windowHeight * 0.65,
-      windowWidth - 400,
-      35
-    );
-    obstacles[1] = new Obstacle(
-      "RECT",
-      0 + 400,
-      windowHeight * 0.35,
-      windowWidth - 400,
-      35
-    ); */
+	obstacles[0] = new Obstacle(
+		'RECT',
+		w / 2 - (w * 0.45) / 2,
+		h * 0.35,
+		w * 0.45,
+		35
+	)
+	obstacles[1] = new Obstacle('RECT', w - 400, h * 0.55, 190, 35)
+	obstacles[2] = new Obstacle('RECT', 200, h * 0.25, 270, 35)
+	obstacles[3] = new Obstacle('RECT', 900, 50, 30, 190)
+	obstacles[4] = new Obstacle('ELLIPSE', w / 2, h * 0.75, 70, 70)
 }
--- sketch.js
+++ sketch.js
@@ -1,292 +1,273 @@
-let population;
-var multiplier = 100000000;
-let lifespan = 800;
-let count = 0;
-let generation = 0;
-let targetSize = 60;
-let alive;
-let completed = 0;
-let crashed = 0;
-let editMode = false;
-let simulating = true;
-let finished = false;
-let canWidth = 800;
-let canHeight = 900;
+let population
+var multiplier = 100000000
+let lifespan = 800
+let count = 0
+let generation = 0
+let targetSize = 60
+let alive
+let completed = 0
+let crashed = 0
+let editMode = false
+let simulating = true
+let finished = false
+let canWidth = 800
+let canHeight = 900
 
 //Edit Mode vars
-let overBox = false;
-let resizeMode = false;
-let editPosMode = false;
-let addMode = true;
-let tempArr = [];
+let overBox = false
+let resizeMode = false
+let editPosMode = false
+let addMode = true
+let tempArr = []
 
 // Menu Trackers
-let genTracker = document.getElementById("generationTracker");
-let avgFitnessTracker = document.getElementById("avgFitnessTracker");
-let lifespanTracker = document.getElementById("livespanTracker");
-let aliveTracker = document.getElementById("aliveTracker");
-let crashedTracker = document.getElementById("crashedTracker");
+let genTracker = document.getElementById('generationTracker')
+let avgFitnessTracker = document.getElementById('avgFitnessTracker')
+let lifespanTracker = document.getElementById('livespanTracker')
+let aliveTracker = document.getElementById('aliveTracker')
+let crashedTracker = document.getElementById('crashedTracker')
+let editModeControls = document.getElementById('editModeControls')
+let statTracker = document.getElementById('statTracker')
 
-let distanceBtn = document.getElementById("distanceBtn");
-let trailBtn = document.getElementById("trailBtn");
-let editBtn = document.getElementById("editBtn");
-let playBtn = document.getElementById("playBtn");
-let populationSlider = document.getElementById("populationSlider");
-let populationValue = document.getElementById("populationValue");
-let lifespanSlider = document.getElementById("lifespanSlider");
-let lifespanValue = document.getElementById("lifespanValue");
+let distanceBtn = document.getElementById('distanceBtn')
+let trailBtn = document.getElementById('trailBtn')
+let editBtn = document.getElementById('editBtn')
+let playBtn = document.getElementById('playBtn')
+let populationSlider = document.getElementById('populationSlider')
+let populationValue = document.getElementById('populationValue')
+let lifespanSlider = document.getElementById('lifespanSlider')
+let lifespanValue = document.getElementById('lifespanValue')
 
-let traget;
-let obstacles;
-let showDistance = false;
-let showTrail = true;
+let target
+let obstacles
+let showDistance = false
+let showTrail = true
 
 function setup() {
-  const canvas = createCanvas(canWidth, canHeight);
-  canvas.parent("container");
-  obstacles = [];
-  createObstacles(canWidth, canHeight);
-  population = new Population();
-  alive = population.size;
-  setupInfos();
-  target = new Target(canWidth / 2, canHeight * 0.1, targetSize);
+	const canvas = createCanvas(canWidth, canHeight)
+	canvas.parent('container')
+	obstacles = []
+	createObstacles(canWidth, canHeight)
+	population = new Population()
+	alive = population.size
+	setupInfos()
+	target = new Target(canWidth / 2, canHeight * 0.1, targetSize)
 }
 
 function draw() {
-  clear();
-  background(0, 100);
-  target.render();
-  //renderInfos();
+	clear()
+	background(0, 100)
+	target.render()
+	renderInfos()
 
-  //_________GAME MODE__________
-  if (!editMode) {
-    if (
-      count == lifespan ||
-      alive == 0 ||
-      crashed + completed == population.size
-    ) {
-      population.evaluate();
-      population.selection();
-      generation++;
-      count = 0;
-      alive = population.size;
-      crashed = 0;
-      completed = 0;
-    }
+	//_________GAME MODE__________
+	if (!editMode) {
+		if (
+			count == lifespan ||
+			alive == 0 ||
+			crashed + completed == population.size
+		) {
+			population.evaluate()
+			population.selection()
+			generation++
+			count = 0
+			alive = population.size
+			crashed = 0
+			completed = 0
+		}
 
-    population.run();
-    if (simulating) {
-      count++;
-    }
-    for (i = 0; i < obstacles.length; i++) {
-      obstacles[i].render();
-    }
-  }
-  //_________EDIT MODE__________
-  if (editMode) {
-    checkOverObstacle();
-    target.checkMouseOver();
-    for (var i = 0; i < tempArr.length; i++) {
-      tempArr[i].render();
-      if (mouseIsPressed && tempArr[i].rezisable && resizeMode) {
-        tempArr[i].width = constrain(mouseX - tempArr[i].x, 20, canWidth);
-        tempArr[i].height = constrain(mouseY - tempArr[i].y, 20, canHeight);
-      }
+		population.run()
+		if (simulating) {
+			count++
+		}
+		for (i = 0; i < obstacles.length; i++) {
+			obstacles[i].render()
+		}
+	}
+	//_________EDIT MODE__________
+	if (editMode) {
+		checkOverObstacle()
+		target.checkMouseOver()
+		for (var i = 0; i < tempArr.length; i++) {
+			tempArr[i].render()
+			if (mouseIsPressed && tempArr[i].rezisable && resizeMode) {
+				tempArr[i].width = constrain(
+					mouseX - tempArr[i].x,
+					20,
+					canWidth
+				)
+				tempArr[i].height = constrain(
+					mouseY - tempArr[i].y,
+					20,
+					canHeight
+				)
+			}
 
-      if (mouseIsPressed && tempArr[i].dragabele && editPosMode) {
-        tempArr[i].x = mouseX - xOffset;
-        tempArr[i].y = mouseY - yOffset;
-      }
-    }
-    if (mouseIsPressed && target.dragabele && editPosMode) {
-      target.x = mouseX;
-      target.y = mouseY;
-    }
-  }
+			if (mouseIsPressed && tempArr[i].dragabele && editPosMode) {
+				tempArr[i].x = mouseX - xOffset
+				tempArr[i].y = mouseY - yOffset
+			}
+		}
+		if (mouseIsPressed && target.dragabele && editPosMode) {
+			target.x = mouseX
+			target.y = mouseY
+		}
+	}
 }
 
 function keyPressed() {
-  if (keyCode == 82) {
-    resizeMode = true;
-    editPosMode = false;
-    addMode = false;
-  }
-  if (keyCode == 84) {
-    editPosMode = true;
-    resizeMode = false;
-    addMode = false;
-  }
-  if (keyCode == 65) {
-    addMode = true;
-    editPosMode = false;
-    resizeMode = false;
-  }
+	if (keyCode == 82) {
+		addMode = false
+		resizeMode = true
+		editPosMode = false
+	}
+	if (keyCode == 84) {
+		addMode = false
+		editPosMode = true
+		resizeMode = false
+	}
+	if (keyCode == 65) {
+		addMode = true
+		editPosMode = false
+		resizeMode = false
+	}
 
-  for (var i = 0; i < tempArr.length; i++) {
-    if (tempArr[i].mouseOver && keyCode == 68) {
-      tempArr.splice(i, 1);
-      console.log("moin");
-    }
-  }
+	for (var i = 0; i < tempArr.length; i++) {
+		if (tempArr[i].mouseOver && keyCode == 68) {
+			tempArr.splice(i, 1)
+		}
+	}
 }
 function mousePressed() {
-  if (!overBox && addMode) {
-    tempArr.push(new Obstacle("RECT", mouseX, mouseY, 60, 60));
-  }
-  for (var i = 0; i < tempArr.length; i++) {
-    if (tempArr[i].mouseOver && resizeMode) {
-      tempArr[i].rezisable = true;
-    } else if (tempArr[i].mouseOver && editPosMode) {
-      tempArr[i].dragabele = true;
-      xOffset = mouseX - tempArr[i].x;
-      yOffset = mouseY - tempArr[i].y;
-    }
-  }
-  if (target.mouseOver) {
-    target.dragabele = true;
-  }
+	if (!overBox && addMode) {
+		tempArr.push(new Obstacle('RECT', mouseX, mouseY, 60, 60))
+	}
+	for (var i = 0; i < tempArr.length; i++) {
+		if (tempArr[i].mouseOver && resizeMode) {
+			tempArr[i].rezisable = true
+		} else if (tempArr[i].mouseOver && editPosMode) {
+			tempArr[i].dragabele = true
+			xOffset = mouseX - tempArr[i].x
+			yOffset = mouseY - tempArr[i].y
+		}
+	}
+	if (target.mouseOver) {
+		target.dragabele = true
+	}
 }
 
 function mouseReleased() {
-  for (var i = 0; i < tempArr.length; i++) {
-    tempArr[i].rezisable = false;
-    tempArr[i].dragabele = false;
-  }
-  target.dragabele = false;
+	for (var i = 0; i < tempArr.length; i++) {
+		tempArr[i].rezisable = false
+		tempArr[i].dragabele = false
+	}
+	target.dragabele = false
 }
 
 function checkOverObstacle() {
-  let count = 0;
-  for (var i = 0; i < tempArr.length; i++) {
-    if (tempArr[i].checkMouseOver()) {
-      tempArr[i].mouseOver = true;
-      count++;
-    } else {
-      tempArr[i].mouseOver = false;
-    }
-  }
-  if (count > 0) {
-    overBox = true;
-  } else {
-    overBox = false;
-  }
+	let count = 0
+	for (var i = 0; i < tempArr.length; i++) {
+		if (tempArr[i].checkMouseOver()) {
+			tempArr[i].mouseOver = true
+			count++
+		} else {
+			tempArr[i].mouseOver = false
+		}
+	}
+	if (count > 0) {
+		overBox = true
+	} else {
+		overBox = false
+	}
 }
 //______________________END________________________________________
 
 function setupInfos() {
-  //Buttons
-  distanceBtn.onclick = function () {
-    showDistance = !showDistance;
-    distanceBtn.classList.toggle("border-secondary");
-    distanceBtn.classList.toggle("border-l-8");
-    distanceBtn.classList.toggle("border-l-4");
-  };
-  trailBtn.onclick = function () {
-    trailBtn.classList.toggle("border-l-8");
-    trailBtn.classList.toggle("border-l-4");
-    trailBtn.classList.toggle("border-white");
+	//Buttons
+	distanceBtn.onclick = function () {
+		showDistance = !showDistance
+		distanceBtn.classList.toggle('border-secondary')
+		distanceBtn.classList.toggle('border-l-8')
+		distanceBtn.classList.toggle('border-l-4')
+	}
+	trailBtn.onclick = function () {
+		trailBtn.classList.toggle('border-l-8')
+		trailBtn.classList.toggle('border-l-4')
+		trailBtn.classList.toggle('border-white')
 
-    showTrail = !showTrail;
-  };
+		showTrail = !showTrail
+	}
 
-  editBtn.onclick = function () {
-    playBtn.classList.toggle("inactive");
-    trailBtn.classList.toggle("inactive");
-    distanceBtn.classList.toggle("inactive");
+	editBtn.onclick = function () {
+		playBtn.classList.toggle('inactive')
+		trailBtn.classList.toggle('inactive')
+		distanceBtn.classList.toggle('inactive')
+		editModeControls.classList.toggle('hidden')
+		statTracker.classList.toggle('hidden')
 
-    editBtn.classList.toggle("border-yellow-300");
-    editBtn.classList.toggle("text-yellow-300");
-    editBtn.classList.toggle("animate-pulse");
-    editBtn.classList.toggle("border-l-4");
-    editBtn.classList.toggle("border-l-8");
+		editBtn.classList.toggle('border-yellow-300')
+		editBtn.classList.toggle('text-yellow-300')
+		editBtn.classList.toggle('border-l-4')
+		editBtn.classList.toggle('border-l-8')
 
-    if (!editMode) {
-      tempArr = [...obstacles];
-      obstacles = [];
-    } else if (editMode) {
-      obstacles = [...tempArr];
-      tempArr = [];
-    }
+		if (!editMode) {
+			tempArr = [...obstacles]
+			obstacles = []
+		} else if (editMode) {
+			obstacles = [...tempArr]
+			tempArr = []
+		}
 
-    editMode = !editMode;
-    simulating = !simulating;
-    defaultValues();
-  };
-  playBtn.onclick = function () {
-    if (simulating) {
-      playBtn.innerHTML = "START";
-    } else {
-      playBtn.innerHTML = "STOP";
-    }
-    playBtn.classList.toggle("border-secondary");
-    playBtn.classList.toggle("text-red-400");
-    playBtn.classList.toggle("text-secondary");
+		editMode = !editMode
+		simulating = !simulating
+		defaultValues()
+	}
+	playBtn.onclick = function () {
+		if (simulating) {
+			playBtn.innerHTML = 'START'
+		} else {
+			playBtn.innerHTML = 'STOP'
+		}
+		playBtn.classList.toggle('border-secondary')
+		playBtn.classList.toggle('text-red-400')
+		playBtn.classList.toggle('text-secondary')
 
-    playBtn.classList.toggle("border-l-8");
-    playBtn.classList.toggle("border-l-4");
+		playBtn.classList.toggle('border-l-8')
+		playBtn.classList.toggle('border-l-4')
 
-    simulating = !simulating;
-    defaultValues();
-  };
+		simulating = !simulating
+		defaultValues()
+	}
 
-  populationSlider.oninput = function () {
-    populationValue.innerHTML = populationSlider.value;
-  };
+	populationSlider.oninput = function () {
+		populationValue.innerHTML = populationSlider.value
+	}
 
-  lifespanSlider.oninput = function () {
-    this.lifespan = lifespanSlider.value;
-    console.log(this.lifespan);
-    lifespanValue.innerHTML = lifespanSlider.value;
-  };
+	lifespanSlider.oninput = function () {
+		this.lifespan = lifespanSlider.value
+		console.log(this.lifespan)
+		lifespanValue.innerHTML = lifespanSlider.value
+	}
 }
 
 function defaultValues() {
-  population = new Population();
-  generation = 0;
-  count = 0;
-  alive = population.size;
-  crashed = 0;
-  completed = 0;
-  clear();
+	population = new Population()
+	generation = 0
+	count = 0
+	alive = population.size
+	crashed = 0
+	completed = 0
+	clear()
 }
 
-/* function renderInfos() {
-  if (completed > 0) {
-    multiplier = 20000;
-  }
-  genTracker.innerHTML = "GEN: " + generation;
-  avgFitnessTracker.innerHTML =
-    "AVG FIT: " + floor(population.avgFitness * multiplier);
-  lifespanTracker.innerHTML = "LIFESPANN: " + count + "/" + lifespan;
-  aliveTracker.innerHTML = "ALIVE: " + alive;
-  crashedTracker.innerHTML = "CRASHED: " + crashed;
-
-  // Distance Btns
-  if (showDistance) {
-    distanceBtn.style.background = "green";
-  } else {
-    distanceBtn.style.background = "rgba(247, 248, 245, 0.5)";
-  }
-
-  // Show Trails Btn
-  if (showTrail) {
-    trailBtn.style.background = "green";
-  } else {
-    trailBtn.style.background = "rgba(247, 248, 245, 0.5)";
-  }
-  // Edit Mode Buttons
-  if (editMode) {
-    editBtn.style.background = "orange";
-  } else {
-    editBtn.style.background = "rgba(247, 248, 245, 0.5)";
-  }
-
-  // Play Btn
-  if (simulating) {
-    playBtn.innerHTML = "PAUSE";
-    playBtn.style.background = "red";
-  } else {
-    playBtn.innerHTML = "PLAY";
-    playBtn.style.background = "green";
-  }
-} */
+function renderInfos() {
+	if (completed > 0) {
+		multiplier = 20000
+	}
+	genTracker.innerHTML = 'GEN: ' + generation
+	avgFitnessTracker.innerHTML =
+		'AVG FIT: ' + floor(population.avgFitness * multiplier)
+	lifespanTracker.innerHTML = 'LIFESPANN: ' + count + '/' + lifespan
+	aliveTracker.innerHTML = 'ALIVE: ' + alive
+	crashedTracker.innerHTML = 'CRASHED: ' + crashed
+}
